fa99314c338738e353376ce2380c4b601416e5e4
Aur3m <42558412+Aur3m@users.noreply.github.com>
2021-06-01 02:38:26 +0200

    Update autochannel.py

--- autochannel.py
+++ autochannel.py
@@ -16,10 +16,21 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="c!help for more information"))
    
    bot.run(token)
+    
+@bot.command(name="help")
+async def help(ctx, extension):
+    #print "help" text
+    
+@bot.command(name="create")
+async def help(ctx, extension):
+    #verify if the user using the command has the admin permission
    
 def create_new_channel(id):
+    #create a new channel with the same permissions in the same category with "[NAME] #[NUMBER]+1"
 
 def rename_current_channel(id):
+    #rename a channel with "[NAME] #[NUMBER]-1"
 
 def remove_channel(id):
+    #remove, delete the concerned channel
 
