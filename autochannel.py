062cf50aa0400ef1cde8a346e434dc5b765c9ef7
paulpvc <paul.vigeolas@gmail.com>
2021-06-01 11:54:53 +0200

    petits bugs
    j'ai juste modifié quelques trucs pour pas que le programme plante la première fois qu'on le lance

--- autochannel.py
+++ autochannel.py
@@ -9,29 +9,31 @@ intents = discord.Intents.all()
 intents.members = True
 client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix="c!",intents=intents)
+def foo():
+    pass
+
+bot = commands.Bot(command_prefix="c!",intents=intents, help_command=foo())
 
 @bot.event
 async def on_ready():
+    print("loaded")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="c!help for more information"))
-   
-   bot.run(token)
     
 @bot.command(name="help")
-async def help():
+async def help(ctx):
     #print "help" text
     
 @bot.command(name="create")
-async def create():
+async def create(ctx):
     #verify if the user using the command has the admin permission
     #create a .json with the server id and assign an id to this new autochannel
     
 @bot.command(name="remove")
-async def remove(id):
+async def remove(ctx, id):
     #remove one of the numerous autochannels defined for the server
     
 @bot.command(name="list")
-async def list():
+async def list(ctx):
     #parse the current active autochannels and their ids from the concerned .json of the current server
    
 def create_new_channel(id):
@@ -43,3 +45,4 @@ def rename_current_channel(id):
 def remove_channel(id):
     #remove, delete the concerned channel
 
+bot.run(token)
