8e97351a08f2ef68455a10985c6e223f47df0acb
paulpvc <paul.vigeolas@gmail.com>
2021-06-15 23:36:58 +0200

    Update autochannel.py

--- autochannel.py
+++ autochannel.py
@@ -209,10 +209,10 @@ async def verif_remove(content, before):
             content[before.channel.name[:before.channel.name.find("#")-1]]["count"] -= 1
             await remove_channel(before.channel)
    
-async def checking_delete(category):
+async def checking_delete(category, name):
     for i in category.channels:
-        pass
-        if i.members == 0:
+        if i.name[:i.name.find("#")-1] == name:
+        #if i.members == 0:
             await remove_channel(i)
 
 
